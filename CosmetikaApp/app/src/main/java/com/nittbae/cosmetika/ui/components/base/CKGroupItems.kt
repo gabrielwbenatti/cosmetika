c4b1f4494c39cfff69d92ca575d1595374902e6c
Gabriel Benatti <gabriel@interno.infonacci.com.br>
2024-04-15 16:36:26 -0300

    criado componente CKGroupItems

---
+++ CosmetikaApp/app/src/main/java/com/nittbae/cosmetika/ui/components/base/CKGroupItems.kt
@@ -0,0 +1,41 @@
+package com.nittbae.cosmetika.ui.components.base
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import com.nittbae.cosmetika.ui.theme.CKLayout
+
+@Composable
+fun CKGroupItems(
+    groupTitle: (@Composable () -> Unit)? = null,
+    surfaceColor: Color = MaterialTheme.colorScheme.surfaceContainerLowest,
+    content: @Composable () -> Unit
+) {
+    Column {
+        groupTitle?.invoke()
+        Box(modifier = Modifier.height(CKLayout.V_PADDING / 2))
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(CKLayout.H_PADDING)),
+            color = surfaceColor
+        ) {
+            Column {
+                content()
+            }
+        }
+    }
+}
